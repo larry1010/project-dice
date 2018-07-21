20a86e766186c4cba7ec7176ff7b4a0bd000d4c2
larry1010 <ll@DESKTOP-3HD60R5.localdomain>
2018-07-21 18:14:48 -0400

    Test Jul 21 2018 6:14 PM.

--- assets/script.js
+++ assets/script.js
@@ -26,12 +26,46 @@ $("#feature-link").click(function () {
 // RANDOM NUMBER GENERATOR
 function randomNumberFunction() {
     var localRandomNumber = Math.floor((Math.random() * 6) + 1);
-    console.log("Random Number: " + localRandomNumber);
     $("#random-numbers").append(localRandomNumber);
 }
 
 // =====================================================================================
 // GENERATE BUTTON ON CLICK
 $("#generate-button").click(function () {
-    randomNumberFunction();
+    // ........................................................................
+    // LOOP RANDOM NUMBER FUNCTION
+    $("#random-numbers").html("");
+    for (var i = 0; i < 6; i++) {
+        randomNumberFunction();
+    }
+    checkRandomNumbersDiv();
 });
+// =====================================================================================
+// CONVERT 6 RANDOM NUMBERS INTO A SINGLE NUMBER
+// var lookUpValue = the random 6 numbers generated above;
+function checkRandomNumbersDiv () {
+    var lookUpValue = $("#random-numbers").html()
+    console.log(lookUpValue);
+}
+
+
+// =====================================================================================
+// CONVERT 6 RANDOM NUMBERS INTO A SINGLE NUMBER
+// Use lookUpValue to find the corresponding word in the data set.
+// Any suggestions on how to look up the data? Is the best way simply to put all the data into an array?
+
+// =====================================================================================
+// Password requirements generator.
+
+// string to array, --> Object --> object name is the key --> Lookup
+// .replace /t with a space
+// console.log(i)
+// var res = str.replace("\t", " ");
+// console.log(i.replace("\t", " "));
+// .split(" ")
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+// https://www.w3schools.com/jsref/jsref_replace.asp
+// https://www.w3schools.com/jsref/jsref_split.asp
+// https://www.w3schools.com/js/js_objects.asp
+// https://repl.it/repls/PleasingTrickyBrowser
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
