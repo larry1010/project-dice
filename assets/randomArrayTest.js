d679928197f9d225dd4596e884bdf2a35bdce783
larry1010 <ll@DESKTOP-3HD60R5.localdomain>
2018-07-27 02:31:09 -0400

    Simplified layout for app. Removed extra button.

--- assets/randomArrayTest.js
+++ assets/randomArrayTest.js
@@ -1,7 +1,7 @@
 // GENERATES A SINGLE RANDOM NUMBER BETWEEN 1 AND 6
 function randomNumberFunction() {
-  var localRandomNumber = Math.floor(Math.random() * 6 + 1);
-  return localRandomNumber;
+    var localRandomNumber = Math.floor(Math.random() * 6 + 1);
+    return localRandomNumber;
 }
 
 // ...................................................................................
@@ -11,24 +11,24 @@ var randomFiveNumbersArray = [];
 // ...................................................................................
 // GENERATES 5 RANDOM NUMBERS
 function generateFiveRandomNumbers() {
-  // $("#random-numbers").html("");
-  for (var i = 0; i < 5; i++) {
-    var localRandom = randomNumberFunction();
-    // return random;
-    // COMBINE THE 5 RANDOM NUMBERS INTO A SINGLE NUMBER
-    randomFiveNumbersArray.push(localRandom);
-    // return localPushRandom;
-    // randomFiveNumbersArray.join("");
-  }
+    // $("#random-numbers").html("");
+    for (var i = 0; i < 5; i++) {
+        var localRandom = randomNumberFunction();
+        // return random;
+        // COMBINE THE 5 RANDOM NUMBERS INTO A SINGLE NUMBER
+        randomFiveNumbersArray.push(localRandom);
+        // return localPushRandom;
+        // randomFiveNumbersArray.join("");
+    }
 }
 
 // WORKING
 // ...................................................................................
 
 function joinFiveRandomNumbers() {
-  var localJoinRandom = randomFiveNumbersArray.join("");
-  // console.log("localJoinRandom: " + localJoinRandom);
-  return localJoinRandom;
+    var localJoinRandom = randomFiveNumbersArray.join("");
+    // console.log("localJoinRandom: " + localJoinRandom);
+    return localJoinRandom;
 }
 
 // WORKING
@@ -37,65 +37,68 @@ function joinFiveRandomNumbers() {
 var lookupValueArray = [];
 
 function pushFiveNumberStringArray() {
-  var localLookupValue = lookupValueArray.push(returnedLookupValueString);
-  return localLookupValue;
-  // console.log("lookupValueArray: " + pushFiveNumberStringArray());
+    var localLookupValue = lookupValueArray.push(returnedLookupValueString);
+    return localLookupValue;
+    // console.log("lookupValueArray: " + pushFiveNumberStringArray());
 }
 
 // console.log("lookupValue: "+ lookupValue);
 
 // ...................................................................................
 function matchNumberStringToWordlist() {
-  var passphrase = wordList[lookupValueArray];
-  console.log("passphrase: " + passphrase);
-  $("#passphrase").append(passphrase + " ");
+    var passphrase = wordList[lookupValueArray];
+    console.log("passphrase: " + passphrase);
+    $("#passphrase").append(passphrase + " ");
 }
 
 // =====================================================================================
 // GENERATE BUTTON ON CLICK
 
-$("#generate-button").click(function() {
-  userInputNumber = parseInt($("#user-input-number").val());
+$("#generate-button").click(function () {
+    userInputNumber = parseInt($("#user-input-number").val());
 
-  // console.log("randomNumberFunction: " + randomNumberFunction());
+    // console.log("randomNumberFunction: " + randomNumberFunction());
 
-  for (i = 0; i < userInputNumber; i++) {
-    generateFiveRandomNumbers();
-    console.log("randomFiveNumbersArray: " + randomFiveNumbersArray);
+    for (i = 0; i < userInputNumber; i++) {
+        generateFiveRandomNumbers();
+        console.log("randomFiveNumbersArray: " + randomFiveNumbersArray);
 
-    joinFiveRandomNumbers();
-    // Why does declaring "var" not work the the following line???
-    // var returnedLookupValueString = joinFiveRandomNumbers();
-    returnedLookupValueString = joinFiveRandomNumbers();
-    console.log("returnedLookupValueString: " + returnedLookupValueString);
+        joinFiveRandomNumbers();
+        // Why does declaring "var" not work the the following line???
+        // var returnedLookupValueString = joinFiveRandomNumbers();
+        returnedLookupValueString = joinFiveRandomNumbers();
+        console.log("returnedLookupValueString: " + returnedLookupValueString);
 
-    pushFiveNumberStringArray();
-    console.log("lookupValueArray: " + lookupValueArray);
+        pushFiveNumberStringArray();
+        console.log("lookupValueArray: " + lookupValueArray);
 
-    matchNumberStringToWordlist();
+        matchNumberStringToWordlist();
 
-    randomFiveNumbersArray = [];
-    lookupValueArray = [];
-  }
+        randomFiveNumbersArray = [];
+        lookupValueArray = [];
+    }
+    savePassphrase();
 
-  // randomArray.push(generateRandomNumber());
-  // console.log("randomArray: " + randomArray);
-  // var userInputNumber = parseInt($("#user-input-number").val());
-  // inputValidation();
-  // for (i = 0; i < userInputNumber; i++) {
-  // ........................................................................
-  // LOOP RANDOM NUMBER FUNCTION
-  // var returnedArrayVar = generateRandomNumbergenerateRandomNumber();
+    // randomArray.push(generateRandomNumber());
+    // console.log("randomArray: " + randomArray);
+    // var userInputNumber = parseInt($("#user-input-number").val());
+    // inputValidation();
+    // for (i = 0; i < userInputNumber; i++) {
+    // ........................................................................
+    // LOOP RANDOM NUMBER FUNCTION
+    // var returnedArrayVar = generateRandomNumbergenerateRandomNumber();
 });
 
 // =====================================================================================
-$("#clear-button").click(function() {
-  // ........................................................................
-  // LOOP RANDOM NUMBER FUNCTION
-  var currentPassphrase = $("#passphrase").html();
-  if (currentPassphrase !== "") {
-    $("#generated-passphrases").prepend(currentPassphrase + "<hr>");
-    $("#passphrase").html("");
-    $("#user-input-number").val("");
-  }
-});
+function savePassphrase() {
+    // ........................................................................
+    // LOOP RANDOM NUMBER FUNCTION
+    var currentPassphrase = $("#passphrase").html();
+    if (currentPassphrase !== "") {
+        $("#passphrase-save").prepend("<div>"+currentPassphrase+"</div><hr>");
+        // $("#generated-passphrases").prepend(currentPassphrase + "<hr>");
+        $("#passphrase").html("");
+        $("#user-input-number").val("");
+    }
+}
+// });
