6ca441681dd3c9d4f324c5fc400b8cc524352808
larry1010 <ll@DESKTOP-3HD60R5.localdomain>
2018-07-27 12:00:27 -0400

    Working validation for input field.

---
+++ assets/randomArrayTest.js
@@ -0,0 +1,131 @@
+// GENERATES A SINGLE RANDOM NUMBER BETWEEN 1 AND 6
+
+function checkInputFieldNumber() {
+    var inputFieldNumber = $("#user-input-number").val();
+    if (inputFieldNumber == "") {
+        $("#error-container").html("");
+    } else if (inputFieldNumber < 6 || inputFieldNumber > 50) {
+        $("#error-container").html("Number out of range.");
+        return false;
+    }
+}
+
+function clearErrorContainer() {
+    $("#error-container").html("");
+}
+
+function clearInputField() {
+    $("#user-input-number").val("");
+    if ($("#error-container").length > 1) {
+        // Do nothing
+    }
+}
+
+function randomNumberFunction() {
+    var localRandomNumber = Math.floor(Math.random() * 6 + 1);
+    return localRandomNumber;
+}
+
+// ...................................................................................
+
+var randomFiveNumbersArray = [];
+
+// ...................................................................................
+// GENERATES 5 RANDOM NUMBERS
+function generateFiveRandomNumbers() {
+    // $("#random-numbers").html("");
+    for (var i = 0; i < 5; i++) {
+        var localRandom = randomNumberFunction();
+        // return random;
+        // COMBINE THE 5 RANDOM NUMBERS INTO A SINGLE NUMBER
+        randomFiveNumbersArray.push(localRandom);
+        // return localPushRandom;
+        // randomFiveNumbersArray.join("");
+    }
+}
+
+// WORKING
+// ...................................................................................
+
+function joinFiveRandomNumbers() {
+    var localJoinRandom = randomFiveNumbersArray.join("");
+    // console.log("localJoinRandom: " + localJoinRandom);
+    return localJoinRandom;
+}
+
+// WORKING
+// ...................................................................................
+
+var lookupValueArray = [];
+
+function pushFiveNumberStringArray() {
+    var localLookupValue = lookupValueArray.push(returnedLookupValueString);
+    return localLookupValue;
+    // console.log("lookupValueArray: " + pushFiveNumberStringArray());
+}
+
+// console.log("lookupValue: "+ lookupValue);
+
+// ...................................................................................
+function matchNumberStringToWordlist() {
+    var passphrase = wordList[lookupValueArray];
+    console.log("passphrase: " + passphrase);
+    $("#passphrase").append(passphrase + " ");
+}
+
+// =====================================================================================
+// GENERATE BUTTON ON CLICK
+
+$("#generate-button").click(function () {
+
+    if (checkInputFieldNumber() == false) {
+        clearInputField(); // Do nothing.
+    } else {
+        clearErrorContainer();
+        userInputNumber = parseInt($("#user-input-number").val());
+
+        // console.log("randomNumberFunction: " + randomNumberFunction());
+
+        for (i = 0; i < userInputNumber; i++) {
+            generateFiveRandomNumbers();
+            console.log("randomFiveNumbersArray: " + randomFiveNumbersArray);
+
+            joinFiveRandomNumbers();
+            // Why does declaring "var" not work the the following line???
+            // var returnedLookupValueString = joinFiveRandomNumbers();
+            returnedLookupValueString = joinFiveRandomNumbers();
+            console.log("returnedLookupValueString: " + returnedLookupValueString);
+
+            pushFiveNumberStringArray();
+            console.log("lookupValueArray: " + lookupValueArray);
+
+            matchNumberStringToWordlist();
+
+            randomFiveNumbersArray = [];
+            lookupValueArray = [];
+        }
+        savePassphrase();
+
+        // randomArray.push(generateRandomNumber());
+        // console.log("randomArray: " + randomArray);
+        // var userInputNumber = parseInt($("#user-input-number").val());
+        // inputValidation();
+        // for (i = 0; i < userInputNumber; i++) {
+        // ........................................................................
+        // LOOP RANDOM NUMBER FUNCTION
+        // var returnedArrayVar = generateRandomNumbergenerateRandomNumber();
+    }
+
+    // =====================================================================================
+    function savePassphrase() {
+        // ........................................................................
+        // LOOP RANDOM NUMBER FUNCTION
+        var currentPassphrase = $("#passphrase").html();
+        if (currentPassphrase !== "") {
+            $("#passphrase-save").prepend("<div>" + currentPassphrase + "</div><hr>");
+            // $("#generated-passphrases").prepend(currentPassphrase + "<hr>");
+            $("#passphrase").html("");
+            $("#user-input-number").val("");
+        }
+    }
+});
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,184 @@
 // =====================================================================================
 // ON LOAD
-$("#passphrase-generator-container").hide();
+// $("#passphrase-generator-container").show();
 $("#resources-container").hide();
+$("#concept-container").hide();
 
 // =====================================================================================
 // NAVIGATION
-function hideResourcesContainer() {
-    $("#resources-container").show();
-    $("#passphrase-generator-container").hide();
-}
-
 function hidePassphraseContainer() {
-    $("#passphrase-generator-container").show();
-    $("#resources-container").hide();
+  $("#passphrase-generator-container").show();
+  $("#generated-passphrases-conatiner").show();
+  $("#generated-passphrases").show();
+  $("#passphrases-title").show();
+  $("#resources-container").hide();
+  $("#concept-container").hide();
 }
 
-$("#resources-link").click(function () {
-    hideResourcesContainer();
-});
-
-$("#feature-link").click(function () {
-    hidePassphraseContainer();
-});
-// =====================================================================================
-// RANDOM NUMBER GENERATOR
-function randomNumberFunction() {
-    var localRandomNumber = Math.floor((Math.random() * 6) + 1);
-    console.log("Random Number: " + localRandomNumber);
+function hideConceptContainer() {
+  $("#concept-container").show();
+  $("#resources-container").hide();
+  $("#passphrase-generator-container").hide();
+  $("#generated-passphrases-conatiner").hide();
+  $("#generated-passphrases").hide();
+  $("#passphrases-title").hide();
 }
 
-// randomNumberFunction();
-$("#random-numbers").html();
-// =====================================================================================
-// GENERATE BUTTON ON CLICK
-$("#generate-button").click(function () {
-    randomNumberFunction();
-    
+function hideResourcesContainer() {
+  $("#resources-container").show();
+  $("#passphrase-generator-container").hide();
+  $("#concept-container").hide();
+  $("#generated-passphrases-conatiner").hide();
+  $("#generated-passphrases").hide();
+  $("#passphrases-title").hide();
+}
+
+$("#resources-link").click(function() {
+  hideResourcesContainer();
 });
+
+$("#feature-link").click(function() {
+  hidePassphraseContainer();
+});
+
+$("#concept-link").click(function() {
+  hideConceptContainer();
+});
+
+// // =====================================================================================
+// // RANDOM NUMBER GENERATOR
+// function randomNumberFunction() {
+//   var localRandomNumber = Math.floor(Math.random() * 6 + 1);
+//   $("#random-numbers").append(localRandomNumber);
+//   return localRandomNumber;
+// }
+
+// // ?????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+// // Why is it so hard to grab the number here and stick it somewhere else? Generate button click needs this value.
+// // function grabUserInputNumber() {
+// //     var userInputNumber = $("#user-input-number").val();
+// //     console.log(userInputNumber);
+// //     return userInputNumber;
+// // }
+// var randomArray = [];
+// // return randomArray;
+
+// function generateRandomNumber() {
+//   $("#random-numbers").html("");
+//   for (var i = 0; i < 5; i++) {
+//     var random = randomNumberFunction();
+//     console.log("generateRandomNumber: " + random);
+//     return random;
+//   }
+// }
+
+// function inputValidation() {
+//   userInputNumber = parseInt($("#user-input-number").val());
+//   if (isNaN(userInputNumber)) {
+//     $("#error-container").html(
+//       "<div style='background-color:red; color:yellow;'>" +
+//         "[Error: User input is not a number.]" +
+//         "</div>"
+//     );
+//     $("#user-input-number").val("");
+//   } else {
+//     $("#error-container").html("");
+//   }
+// }
+
+// // =====================================================================================
+// // GENERATE BUTTON ON CLICK
+
+// $("#generate-button").click(function() {
+//   randomArray.push(generateRandomNumber());
+//   console.log("randomArray: " + randomArray);
+//   // var userInputNumber = parseInt($("#user-input-number").val());
+//   inputValidation();
+//   for (i = 0; i < userInputNumber; i++) {
+//     // ........................................................................
+//     // LOOP RANDOM NUMBER FUNCTION
+//     // var returnedArrayVar = generateRandomNumbergenerateRandomNumber();
+//     randomNumberFunction();
+//     generateRandomNumber();
+//     checkRandomNumbersDiv();
+//   }
+// });
+
+// // =====================================================================================
+// // SAVE AND CLEAR BUTTON ON CLICK
+// $("#clear-button").click(function() {
+//   var blankCheck = $("#random-numbers").html();
+//   if (blankCheck == "") {
+//   } else {
+//     // ........................................................................
+//     // LOOP RANDOM NUMBER FUNCTION
+//     var currentPassphrase = $("#passphrase").html();
+//     $("#generated-passphrases").prepend(currentPassphrase + "<hr>");
+//     $("#random-numbers").html("");
+//     $("#passphrase").html("");
+//     $("#user-input-number").val("");
+//   }
+// });
+
+// // =====================================================================================
+// // CONVERT 6 RANDOM NUMBERS INTO A SINGLE NUMBER
+// // var lookUpValue = the random 6 numbers generated above;
+// function checkRandomNumbersDiv() {
+//   // var lookUpValue = $("#random-numbers").html();
+//   var lookUpValue = 111111;
+//   // ...........................................................................
+//   // Append word from lookUpValue
+//   var passphrase = wordList[lookUpValue];
+//   $("#passphrase").append(passphrase + " ");
+// }
+
+// // =====================================================================================
+// // randomNumberFunction();
+
+// //Generate a random bit
+// // $("#random-bit-generator").click(function () {
+// // alert('test 2');
+// // $("#random-bit-generator output").first().attr('value', 20);
+// // document.getElementById('bit_output').value = 'test';
+// // });
+
+// =====================================================================================
+// CONVERT 6 RANDOM NUMBERS INTO A SINGLE NUMBER
+// Use lookUpValue to find the corresponding word in the data set.
+// Any suggestions on how to look up the data? Is the best way simply to put all the data into an array?
+
+// =====================================================================================
+// Password requirements generator.
+
+// https://www.w3schools.com/js/js_strings.asp
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+// string to array, --> Object --> object name is the key --> Lookup
+// .replace /t with a space
+// console.log(i)
+// var res = str.replace("\t", " ");
+// console.log(i.replace("\t", " "));
+// .split(" ")
+
+// https://www.w3schools.com/jsref/jsref_replace.asp
+// https://www.w3schools.com/jsref/jsref_split.asp
+
+// https://www.w3schools.com/js/js_objects.asp
+
+// https://repl.it/repls/PleasingTrickyBrowser
+// https://repl.it/repls/UnlinedCrazyRam
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+
+// =====================================================================================
+// Git Resources
+// https://www.atlassian.com/git/tutorials/resetting-checking-out-and-reverting
+
+// =====================================================================================
+// Convert list to JSON
+// Used Excel to convert list to CSV.
+// Used CSV to JSON online converter to convert CSV to JSON had to use transpose to get object in key:value pair.
+// https://www.csvjson.com/csv2json
